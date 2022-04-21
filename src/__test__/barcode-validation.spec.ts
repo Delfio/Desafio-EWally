1b2840cb475176ce1a4a6e67be3cb59ba9f39cb3
Delfio Francisco <delfio_eu@hotmail.com>
2022-04-21 09:41:37 -0400

    test: primeiros testes responsáveis por aplicar regra de negócio na validação do código de barras do boleto

---
+++ src/__test__/barcode-validation.spec.ts
@@ -0,0 +1,5 @@
+describe("Testes responsáveis por aplicar regra de negócio sobre a validação do código de barras", () => {
+    test.todo("Espero que seja possível validar um código de barras");
+    test.todo("Caso o código de barras seja inválido, um erro deverá ser retornado");
+    test.todo("Caso o código de barras seja válido, sucesso deverá ser retornado");
+})
