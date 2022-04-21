6f6116dcd791fc7d386b90e0a9c7c017ffa6802f
Delfio Francisco <delfio_eu@hotmail.com>
2022-04-21 09:47:37 -0400

    test: primeiros testes responsáveis por aplicar regra de negócio na decodificação do valor do boleto

---
+++ src/__test__/decode-value.spec.ts
@@ -0,0 +1,3 @@
+describe("Testes responsáveis por aplicar regra de negócio sobre a decodificação do valor do boleto", () => {
+    test.todo("Espero que seja possível decodificar o valor do boleto");
+})
