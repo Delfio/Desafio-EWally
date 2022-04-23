5c42f4c35691c7871096e2c60d0c24cffebfeded
Delfio Francisco <delfio_eu@hotmail.com>
2022-04-23 11:07:15 -0400

    refactor: validação do código verificador do campo do boleto

--- src/__test__/ValidateCheckDigitInBarcodeField.spec.ts
+++ src/__test__/ValidateCheckDigitInBarcodeField.spec.ts
@@ -43,4 +43,31 @@ describe("Testes responsáveis por validar a regra de negocio da validação do 
     expect(response.check_digit).toEqual(request.campo.dv);
     expect(response.is_valid).toBe(true);
   });
+
+  test("Espero que a validação me retorne que o código não é valido", async () => {
+    const validateCheckDigitInBarcodeField =
+      new ValidateCheckDigitInBarcodeField();
+
+    const request = {
+      campo: {
+        dv: '2',
+        str_barcode_field: "237933812",
+      },
+    };
+
+    /**
+     * {
+      codigo_moeda: '9',
+      dv: '8',
+      instituicao_destinataria: '237',
+      posicao_20_24: '33812',
+      str_barcode_field: '237933812'
+    }
+     */
+    const response = await validateCheckDigitInBarcodeField.handle(request);
+
+    expect(response).not.toBeNull();
+    expect(response.check_digit).toEqual(request.campo.dv);
+    expect(response.is_valid).toBe(false);
+  });
 });
--- src/implementation/services/ValidateCheckDigitInBarcodeField.ts
+++ src/implementation/services/ValidateCheckDigitInBarcodeField.ts
@@ -3,27 +3,80 @@ import {
   IBarcodeTypeB,
   IBarcodeTypeC,
   IBarcodeTypeD,
-  IBarcodeTypeE
-} from '../../domain/entities/TypeBarcode';
-import IValidateCheckDigitInBarcodeField, { IRequest, IResponse } from "../../domain/services/IValidateCheckDigitInBarcodeField";
+  IBarcodeTypeE,
+} from "../../domain/entities/TypeBarcode";
+import IValidateCheckDigitInBarcodeField, {
+  IRequest,
+  IResponse,
+} from "../../domain/services/IValidateCheckDigitInBarcodeField";
 
-class ValidateCheckDigitInBarcodeField implements IValidateCheckDigitInBarcodeField {
+class ValidateCheckDigitInBarcodeField
+  implements IValidateCheckDigitInBarcodeField
+{
   async handle(req: IRequest): Promise<IResponse> {
-    const {
-      str_barcode_field,
-      dv
-    } = req.campo;
+    const { str_barcode_field, dv } = req.campo;
 
-    const result = await this.validateField(str_barcode_field);
+    const result = await this.validateField(str_barcode_field, dv);
 
     return {
       check_digit: dv,
-      is_valid: result
-    }
+      is_valid: result,
+    };
   }
 
-  private async validateField(str_field: string): Promise<boolean> {
-    return Promise.resolve(false);
+  private async validateField(str_field: string, dv: string): Promise<boolean> {
+    const multipliers = (() => {
+      if (str_field.length % 2 > 0) {
+        return {
+          et1: 2,
+          et2: 1,
+        };
+      }
+
+      return {
+        et1: 1,
+        et2: 2,
+      };
+    })();
+
+    function algorithmsMultiplier(algorithm: number): number {
+      if (algorithm <= 9) {
+        return algorithm;
+      }
+
+      const result =
+        Number(String(algorithm)[0]) + Number(String(algorithm)[1]);
+
+      return algorithmsMultiplier(result);
+    }
+
+    const all_numbers_added = Array.from({
+      length: str_field.length,
+    })
+      .map((_, index) => Number(str_field[index]))
+      .map((value, index) => {
+        let result = 0;
+        if ((index + 1) % 2 === 0) {
+          result = value * multipliers.et2;
+        } else {
+          result = value * multipliers.et1;
+        }
+
+        result = algorithmsMultiplier(result);
+
+        return result;
+      })
+      .reduce((prev, curr) => {
+        return prev + curr;
+      }, 0);
+
+    const module_of_1o = all_numbers_added % 10;
+    const next_ten = Math.ceil(module_of_1o / 10) * 10;
+
+    const rest = next_ten - module_of_1o;
+    const rest_module_of_10 = String(rest % 10);
+
+    return Promise.resolve(rest_module_of_10 === dv);
   }
 }
 
