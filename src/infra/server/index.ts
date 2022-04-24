95f4dbf8a4692b04e696de4463eb44ce7ae26cf3
Delfio Francisco <delfio_eu@hotmail.com>
2022-04-24 10:19:00 -0400

    feat: documentação da api - swagger ui

--- src/infra/server/index.ts
+++ src/infra/server/index.ts
@@ -1,20 +1,28 @@
-import Express from 'express';
-import routes from '../routes';
+import Express from "express";
+import routes from "../routes";
+import swaggerUi from "swagger-ui-express";
+import ApiDoc from "../../../docs/API-DOC.json";
 
 class ExpressServer {
-    private server: Express.Express;
-    constructor() {
-        this.server = Express();
-    }
+  private server: Express.Express;
+  constructor() {
+    this.server = Express();
+  }
 
-    private configureRoutes() {
-        routes(this.server);
-    }
+  private configureRoutes() {
+    routes(this.server);
+  }
 
-    runServer() {
-        this.configureRoutes();
-        this.server.listen(3333, () => console.log("Server is onlyne as 3333 🤖"))
-    }
+  private configureServerDocs() {
+    this.server.use("/api-doc", swaggerUi.serve);
+    this.server.get("/api-doc", swaggerUi.setup(ApiDoc));
+  }
+
+  runServer() {
+    this.configureRoutes();
+    this.configureServerDocs();
+    this.server.listen(3333, () => console.log("Server is onlyne as 3333 🤖"));
+  }
 }
 
 export default ExpressServer;
